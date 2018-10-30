9eea840b58f48bca05c813e015cb78f0a29d2d2e
Rok Dolinar <dolinar.rok0@gmail.com>
2018-10-30 21:41:15 +0100

    added password validation (at least one special char and one digit)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Rules\HasAtLeastOneDigit;
+use App\Rules\HasAtLeastOneSpecialChar;
 
 class RegisterController extends Controller
 {
@@ -51,7 +53,8 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => ['required', 'string', 'min:8', 'confirmed', new HasAtLeastOneDigit, new HasAtLeastOneSpecialChar],
+            //'password' => 'required|string|min:8|confirmed',
         ]);
     }
 
--- resources/lang/sl/auth.php
+++ resources/lang/sl/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Uporabniško ime in/ali geslo je napačno.',
+    'throttle' => 'Preveč neuspelih poskusov prijave. Poskusite znova čez :seconds sekund.',
 
 ];
